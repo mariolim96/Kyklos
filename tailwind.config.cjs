19939e23210a06b7596faca724e9937e51abc33f
Mario Esposito <mario.esposito@abstract.it>
2023-03-12 17:19:55 +0100

    feat: added atom components

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,9 +4,63 @@ const config = {
   theme: {
     extend: {},
   },
-  plugins: [
-    require("@tailwindcss/typography"), require("daisyui")
-  ],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        default: {
+          primary: "#0D121D",
+          secondary: "#121A29",
+          accent: "#8B5CF6",
+          neutral: "#767F92",
+          "base-100": "#F1F2F9",
+          info: "#2187D0",
+          success: "#25CE8F",
+          warning: "#FBBD23",
+          error: "#F45353",
+          "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
+          "--rounded-btn": "5rem", // border radius rounded-btn utility class, used in buttons and similar element
+          "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
+          "--animation-btn": "0.25s", // duration of animation when you click on button
+          "--animation-input": "0.2s", // duration of animation for inputs like checkbox, toggle, radio, etc
+          "--btn-text-case": "uppercase", // set default text transform for buttons
+          "--btn-focus-scale": "0.95", // scale transform of button when you focus on it
+          "--border-btn": "1px", // border width of buttons
+          "--tab-border": "1px", // border width of tabs
+          "--tab-radius": "0.5rem",
+        },
+      },
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      "halloween",
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+    ],
+  },
 };
 
 module.exports = config;
